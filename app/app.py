06740ecf7d7e6b7f7f6cf4b3d0775eefc2bea55b
sgkertsos <165837855+sgkertsos@users.noreply.github.com>
2024-12-27 10:43:54 +0000

    Changes made.

--- app/app.py
+++ app/app.py
@@ -2,6 +2,10 @@ import streamlit as st
 from predict_service_functions import predict_traffic_sign
 
 def main():
+    # Set browser tab title
+    st.set_page_config(page_title="Traffic sign classification app", menu_items=None, page_icon="random")
+
+    # Select a file to upload to the classification web service
     uploaded_file = st.file_uploader("Choose a file", ['jpg', 'png'])
     if uploaded_file is not None:
         response = predict_traffic_sign(uploaded_file)
