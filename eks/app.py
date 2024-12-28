017fd6de5568d946e1a75740c22417855b30ec2e
sgkertsos <165837855+sgkertsos@users.noreply.github.com>
2024-12-28 19:25:19 +0000

    Some improvements made.

---
+++ eks/app.py
@@ -0,0 +1,18 @@
+import streamlit as st
+from predict_service_functions import predict_traffic_sign
+
+def main():
+    # Set browser tab title
+    st.set_page_config(page_title="Traffic sign classification app (EKS Version)", menu_items=None, page_icon="random")
+    
+    # Set page title
+    st.markdown('### Traffic sign classification app (EKS Version)')
+
+    # Select a file to upload to the classification web service
+    uploaded_file = st.file_uploader("Choose a file", ['jpg', 'png'])
+    if uploaded_file is not None:
+        response = predict_traffic_sign(uploaded_file)
+        st.write(f"Class Index is {response['class_index']} and class name is {response['class_name']}")
+
+if __name__ == "__main__":
+    main()
--- eks/test.py
+++
@@ -1,8 +0,0 @@
-from predict_service_functions import predict_traffic_sign
-
-image = '../app/data/test/00002.png'
-
-response = predict_traffic_sign(image)
-
-print(f"Class Index is {response['class_index']} and class name is {response['class_name']}")
-
